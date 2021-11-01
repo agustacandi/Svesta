b7c493575aac76917d94f7112f0fb25abb9cfa86
agustacandi <mugiwaraagusta@gmail.com>
2021-11-01 23:10:03 +0700

    typo 2

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to Svesta</h1>
+<p>Powered by <a href="https://kit.svelte.dev">Svelte-Kit</a> and <a href="https://vercel.com">Vercel</a></p>
