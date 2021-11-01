f8549e3d56615f6dc3932ab905c508176d2cb67c
agustacandi <mugiwaraagusta@gmail.com>
2021-11-01 22:54:39 +0700

    init

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,12 @@
+<nav>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+</nav>
+
+<slot></slot>
+
+<style>
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
+</style>
